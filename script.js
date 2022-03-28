815115a21671769a0de2e104259b77d4505cef43
zal626 <riabdulsali@gmail.com>
2022-03-28 19:45:15 +0800

    Add function computerPlay to js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 function computerPlay(){
-    
+    const computerOptions = ['rock', 'paper', 'scissor'];
+    const computerPick = Math.floor(Math.random() * 3);
+    const computerChoice = computerOptions[computerPick];
+    return computerChoice;
 }
