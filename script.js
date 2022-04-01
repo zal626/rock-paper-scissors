e61559721a4f4784fc920b5072002dce4f7a74ae
zal626 <riabdulsali@gmail.com>
2022-04-01 16:17:37 +0800

    Add playRound function

--- script.js
+++ script.js
@@ -4,3 +4,35 @@ function computerPlay(){
     const computerChoice = computerOptions[computerPick];
     return computerChoice;
 }
+function playRound (playerSelection, computerSelection){
+    if (computerSelection === playerSelection){
+        return 'It is a tie!';
+    }
+    if(playerSelection === 'rock'){
+        if(computerSelection === 'paper'){
+            return 'Computer win!';
+        }
+        else{
+            return 'Player win!';
+        }
+    }
+    if(playerSelection === 'scissor'){
+        if(computerSelection === 'paper'){
+            return 'Player win!';
+        }
+        else{
+            return 'Computer win!';
+        }
+    }
+    if(playerSelection === 'paper'){
+        if(computerSelection === 'scissor'){
+            return 'Computer win!';
+        }
+        else{
+            return 'Player win!';
+        }
+    }
+}
+const playerSelection = prompt('Do you choose rock, paper, or scissor?').toLowerCase();
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
